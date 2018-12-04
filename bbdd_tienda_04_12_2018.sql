6298bea6bb9c5a0eacb148be0298c60655ed2d43
MagarciaM <36037048+MagarciaM@users.noreply.github.com>
2018-12-04 21:25:10 +0100

    sql

--- bbdd_tienda_04_12_2018.sql
+++ bbdd_tienda_04_12_2018.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Versión del servidor:         5.7.21-log - MySQL Community Server (GPL)
 -- SO del servidor:              Win64
--- HeidiSQL Versión:             9.5.0.5339
+-- HeidiSQL Versión:             9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `articulo` (
   `id_categoria_art` int(11) NOT NULL,
   `id_subcategoria_art` int(11) NOT NULL,
   `nombre_art` varchar(50) NOT NULL,
-  `descripcion_art` varchar(500) NOT NULL,
+  `descripcion_art` varchar(50) NOT NULL,
   `importe_art` float NOT NULL,
   `fecha_alta_art` datetime NOT NULL,
   `cantidad_max_art` int(11) NOT NULL,
@@ -52,18 +52,13 @@ CREATE TABLE IF NOT EXISTS `articulo` (
   CONSTRAINT `FK_articulo_categoria` FOREIGN KEY (`id_categoria_art`) REFERENCES `categoria` (`id`),
   CONSTRAINT `FK_articulo_subcategoria` FOREIGN KEY (`id_subcategoria_art`) REFERENCES `subcategoria` (`id`),
   CONSTRAINT `FK_articulo_vendedor` FOREIGN KEY (`id_vendedor_art`) REFERENCES `vendedor` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.articulo: ~7 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.articulo: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `articulo` DISABLE KEYS */;
-REPLACE INTO `articulo` (`id`, `id_vendedor_art`, `id_categoria_art`, `id_subcategoria_art`, `nombre_art`, `descripcion_art`, `importe_art`, `fecha_alta_art`, `cantidad_max_art`, `imagen_art`) VALUES
-	(1, 1, 1, 1, 'Abeja Reina', 'De color pajizo (casi dorado) y con una retención de espuma extraña en cervezas de su estilo, de trigo bávaras. Aromas cítricos con notas florales y recuerdos a miel y especias. Una receta basada en las cervezas de trigo alemanas tradicionales pero con una carga extra de lúpulos americanos.', 2.5, '2018-11-27 00:00:00', 150, 'images/products/AbejaReina.jpg'),
-	(2, 1, 1, 1, 'Marijuana', 'La única cerveza cerveza artesana que podrás encontrar actualmente con un toque de tequila y cáñamo. Una cerveza "muy canalla" de tipo Pale Ale con tequila y cáñamo. Un sabor sorprendente gracias a la combinación del lúpulo y el cáñamo, ambos de la familia de las Cannabáceas. Suave, fácil de beber, de trago largo y muy refrescante.', 2, '2018-11-23 01:05:48', 300, 'images/products/Marijuana.jpg'),
-	(3, 2, 1, 1, 'Bertus White IPA', 'Cerveza de estilo IPA elaborada con trigo, junto a tres tipos de malta y varios lúpulos cítricos americanos. De aspecto amarillo pajizo y con cierta turbidez. Aromas afurtados y sabores cítricos ligeramente acaramelados.', 3, '2018-11-27 23:21:26', 100, 'images/products/BertusWhiteIPA.jpg'),
-	(4, 3, 1, 1, '3Monos Monkey Business', 'Una American Pale Ale equlibrada y llena de matices. De color ambarino y aromas frutales y tropicales. Con un sabor ligeramente dulce (bien integrado con el amargor característico de su estilo) y toques frutales y acaramelados. De cuerpo maltoso con espuma beige y persistente.', 4.5, '2018-11-27 23:22:02', 235, 'images/products/MonkeyBusiness.jpg'),
-	(5, 2, 1, 1, 'Matadragons', 'De color cobrizo y con un cuerpo denso y una espuma moderada. De aromas y sabores ahumados con notas dulces. Gracias a su alta graduación, esta cerveza puede añejarse durante años.', 2.25, '2018-11-27 23:22:44', 124, 'images/products/MataDragons.jpg'),
-	(6, 3, 1, 1, 'Little Bichos Hormiga', 'Una cerveza de estilo Brown Ale elaborada con Trenza riojana, un pastel típico de La Rioja en colaboración con Horno Arguiñano y Eduardo Villar, su maestro pastelero. Te sorprenderá el dulzor de la trenza combinado con el amargor de la cerveza... una experiencia única.', 4, '2018-11-27 23:23:16', 100, 'images/products/LittleBichosHormiga.jpg'),
-	(7, 3, 1, 1, 'Sant Rock', 'Un homenaje a la cerveza trapense de origen belga. En su elaboración se emplean maltas caramelizadas y una pequeña cantidad de azúcar de caña. El resultado es una cerveza dulce, de cuerpo denso, alcohólica y con la malta como principal actriz tanto en nariz como en boca con recuerdos a barrica, pasas y tofe.', 2, '2018-11-27 23:23:43', 20, 'images/products/SantRock.jpg');
+INSERT INTO `articulo` (`id`, `id_vendedor_art`, `id_categoria_art`, `id_subcategoria_art`, `nombre_art`, `descripcion_art`, `importe_art`, `fecha_alta_art`, `cantidad_max_art`, `imagen_art`) VALUES
+	(1, 3, 1, 1, 'Abeja Reina', 'Mu buena', 10, '2018-11-27 00:00:00', 5, 'images/products/AbejaReina.jpg'),
+	(2, 3, 1, 1, 'Marijuana', 'Mu güena tambien', 10, '2018-11-23 01:05:48', 3, 'images/products/Marijuana.jpg');
 /*!40000 ALTER TABLE `articulo` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.categoria
@@ -76,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 
 -- Volcando datos para la tabla bd_tienda.categoria: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-REPLACE INTO `categoria` (`id`, `nombre_cat`, `descripcion_cat`) VALUES
+INSERT INTO `categoria` (`id`, `nombre_cat`, `descripcion_cat`) VALUES
 	(1, 'Rubia', 'Cerveza Rubita');
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 
@@ -93,10 +88,12 @@ CREATE TABLE IF NOT EXISTS `comentario` (
   KEY `FK_comentario_usuario` (`id_usuario_com`),
   CONSTRAINT `FK_comentario_articulo` FOREIGN KEY (`id_articulo_com`) REFERENCES `articulo` (`id`),
   CONSTRAINT `FK_comentario_usuario` FOREIGN KEY (`id_usuario_com`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla bd_tienda.comentario: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+INSERT INTO `comentario` (`id`, `id_articulo_com`, `id_usuario_com`, `descripcion_com`, `valoracion_com`, `fecha_alta_com`) VALUES
+	(1, 1, 9, 'holaaaa', 2, '2018-12-04 20:37:06');
 /*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.direccion
@@ -109,12 +106,15 @@ CREATE TABLE IF NOT EXISTS `direccion` (
   PRIMARY KEY (`id`),
   KEY `FK_direccion_usuario` (`id_usuario`),
   CONSTRAINT `FK_direccion_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.direccion: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.direccion: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `direccion` DISABLE KEYS */;
-REPLACE INTO `direccion` (`id`, `id_usuario`, `direccion`, `poblacion`, `pais`) VALUES
-	(10, 9, 'C/Filipinas 8 1E', 'Albacete', 'EspaÃ±a');
+INSERT INTO `direccion` (`id`, `id_usuario`, `direccion`, `poblacion`, `pais`) VALUES
+	(9, 8, 'C/Filipinas 8 1E', 'Albacete', 'EspaÃ±a'),
+	(10, 8, 'C/ ejemplo ', 'Albacete', 'EspaÃ±a'),
+	(11, 9, 'yo', 'yo', 'yo'),
+	(12, 9, 'la mia', 'la mia', 'espaÃ±a');
 /*!40000 ALTER TABLE `direccion` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.pedido
@@ -133,12 +133,13 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   CONSTRAINT `FK_pedido_direccion` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id`),
   CONSTRAINT `FK_pedido_tarjeta` FOREIGN KEY (`id_tarjeta`) REFERENCES `tarjeta` (`id`),
   CONSTRAINT `FK_pedido_usuario` FOREIGN KEY (`id_usuario_ped`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.pedido: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.pedido: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
-REPLACE INTO `pedido` (`id`, `id_usuario_ped`, `id_tarjeta`, `id_direccion`, `fecha_ped`, `importe_total`, `completado`) VALUES
-	(12, 9, 6, 10, '2018-11-27 23:14:28', 30, 0);
+INSERT INTO `pedido` (`id`, `id_usuario_ped`, `id_tarjeta`, `id_direccion`, `fecha_ped`, `importe_total`, `completado`) VALUES
+	(12, 8, 6, 10, '2018-11-28 19:39:30', 50, 0),
+	(13, 9, 7, 12, '2018-12-04 20:54:06', 20, 1);
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.pedido_lin
@@ -153,13 +154,14 @@ CREATE TABLE IF NOT EXISTS `pedido_lin` (
   KEY `FK_pedido_lin_pedido` (`id_pedido`),
   CONSTRAINT `FK_pedido_lin_articulo` FOREIGN KEY (`id_articulo_lin`) REFERENCES `articulo` (`id`),
   CONSTRAINT `FK_pedido_lin_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.pedido_lin: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.pedido_lin: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `pedido_lin` DISABLE KEYS */;
-REPLACE INTO `pedido_lin` (`id`, `id_pedido`, `id_articulo_lin`, `cantidad_lin`, `importe_lin`) VALUES
-	(17, 12, 2, 2, 20),
-	(18, 12, 1, 1, 10);
+INSERT INTO `pedido_lin` (`id`, `id_pedido`, `id_articulo_lin`, `cantidad_lin`, `importe_lin`) VALUES
+	(17, 12, 2, 5, 50),
+	(18, 13, 1, 1, 10),
+	(19, 13, 2, 1, 10);
 /*!40000 ALTER TABLE `pedido_lin` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.subcategoria
@@ -175,8 +177,8 @@ CREATE TABLE IF NOT EXISTS `subcategoria` (
 
 -- Volcando datos para la tabla bd_tienda.subcategoria: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `subcategoria` DISABLE KEYS */;
-REPLACE INTO `subcategoria` (`id`, `id_categoria`, `nombre_subcat`, `descripcion_subcat`) VALUES
-	(1, 1, 'Ale', 'Larga Fermentación');
+INSERT INTO `subcategoria` (`id`, `id_categoria`, `nombre_subcat`, `descripcion_subcat`) VALUES
+	(1, 1, 'Larga', 'Larga Fermentacion');
 /*!40000 ALTER TABLE `subcategoria` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.tarjeta
@@ -189,19 +191,21 @@ CREATE TABLE IF NOT EXISTS `tarjeta` (
   PRIMARY KEY (`id`),
   KEY `FK_tarjeta_usuario` (`id_usuario_tarj`),
   CONSTRAINT `FK_tarjeta_usuario` FOREIGN KEY (`id_usuario_tarj`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.tarjeta: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.tarjeta: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `tarjeta` DISABLE KEYS */;
-REPLACE INTO `tarjeta` (`id`, `id_usuario_tarj`, `numero_tarj`, `mes_cad_tarj`, `anno_cad_tarj`) VALUES
-	(6, 9, '4344765772807785', 3, 22);
+INSERT INTO `tarjeta` (`id`, `id_usuario_tarj`, `numero_tarj`, `mes_cad_tarj`, `anno_cad_tarj`) VALUES
+	(5, 8, '4232075279495208', 22, 22),
+	(6, 8, '4294785894698565', 2, 18),
+	(7, 9, '4232075279495208', 4, 19);
 /*!40000 ALTER TABLE `tarjeta` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nif_usr` varchar(9) NOT NULL,
-  `pass_usr` varchar(9) NOT NULL,
+  `pass_usr` varchar(200) NOT NULL,
   `nombre_usr` varchar(50) NOT NULL,
   `apellido_usr` varchar(50) NOT NULL,
   `email_usr` varchar(50) NOT NULL,
@@ -211,10 +215,11 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `nif_usr` (`nif_usr`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.usuario: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.usuario: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-REPLACE INTO `usuario` (`id`, `nif_usr`, `pass_usr`, `nombre_usr`, `apellido_usr`, `email_usr`, `fecha_alta_usr`, `fecha_nac_usr`) VALUES
-	(9, '47446804n', '123456789', 'Julian', 'Rodenas', 'cosacosa@cosa.com', '2018-11-27', '2018-11-27');
+INSERT INTO `usuario` (`id`, `nif_usr`, `pass_usr`, `nombre_usr`, `apellido_usr`, `email_usr`, `fecha_alta_usr`, `fecha_nac_usr`) VALUES
+	(8, '47446804n', '123456789', 'Julian', 'Diaz Rodenas', 'cosacosa@cosa.com', '2018-11-26', '2018-11-26'),
+	(9, '48259583V', 'p+0giICy5aSPDxwubhiucg==', 'yo', 'yo', 'yo@yo.com', '2018-12-04', '2000-01-01');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 -- Volcando estructura para tabla bd_tienda.vendedor
@@ -232,12 +237,12 @@ CREATE TABLE IF NOT EXISTS `vendedor` (
   UNIQUE KEY `nif_vend` (`nif_vend`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla bd_tienda.vendedor: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla bd_tienda.vendedor: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `vendedor` DISABLE KEYS */;
-REPLACE INTO `vendedor` (`id`, `nif_vend`, `pass_vend`, `nombre_vend`, `pais_vend`, `poblacion_vend`, `direccion_vend`, `email_vend`, `fecha_alta_vend`) VALUES
-	(1, '123456789', '123456789', 'BirraMan', 'España', 'Albacete', 'C/Industria', 'asd@asd.es', '2018-11-23'),
+INSERT INTO `vendedor` (`id`, `nif_vend`, `pass_vend`, `nombre_vend`, `pais_vend`, `poblacion_vend`, `direccion_vend`, `email_vend`, `fecha_alta_vend`) VALUES
+	(1, '123456789', '123456789', 'Cerveceria1', 'Cerv', '', '', 'asd@asd.es', '2018-11-23'),
 	(2, '789456123', '123456789', 'Granujilla de Jerez', 'Andorra', 'Andorra', 'C/1 Andorra la Bella', 'cosacosa@cosa.com', '2018-11-26'),
-	(3, '987654321', '123456789', 'Hombre Caracol', 'España', 'Sevilla', 'C/Giralda', 'cosa@yahoo.com', '2018-11-27');
+	(3, '48259584H', 'PLS1wlU6Ea0zgd0GbRSy1g==', 'yo', 'yo', 'yo', 'yo', 'yo@yo.com', '2018-12-04');
 /*!40000 ALTER TABLE `vendedor` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
